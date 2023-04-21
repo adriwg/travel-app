41303d904211bc9411b52f25f2898645c94035d4
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-04-21 20:52:47 +0100

    implemented daterange picker

--- datepicker.js
+++ datepicker.js
@@ -1,4 +1,18 @@
-// $('.input-daterange input').each(function() {
-//     $(this).datepicker('clearDates');
-// });
 
+$(function() {
+    $('#date-range-picker').daterangepicker({
+      opens: "left",
+      applyClass: "btn-primary",
+      cancelClass: "btn-default",
+      
+    }, function(start, end, label) {
+      console.log("A new date range was chosen: " + start.format('DD/MM/YYYY') + ' to ' + end.format('DD/MM/YYYY'));
+      let arrival = start.format('DD/MM/YYYY');
+      let departure = end.format('DD/MM/YYYY');
+      console.log(typeof(arrival), arrival);
+      console.log(typeof(departure), departure);
+    });
+
+
+  });
+  
