b9c42db949a1864d4b418a1828ad1a37d3b2d280
AdrianWong <adrianwg04@gmail.com>
2023-04-21 17:31:15 +0100

    Revise the code for the changes of the Booking api. Add function to display the hotels dynamically.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var des = "London";
-var arrival_date = "2023-04-20";
+var des = "Birmingham";
+var arrival_date = "2023-04-22";
 var departure_date = "2023-04-25";
 var hotels = [];
 var hotel_images = [];
 var searched_cities = [];
-var baseURL_desInfo = "https://apidojo-booking-v1.p.rapidapi.com/locations/auto-complete?";
-var baseURL_hotels = "https://apidojo-booking-v1.p.rapidapi.com/properties/list?";
+var baseURL_desInfo = "https://booking-com.p.rapidapi.com/v1/hotels/locations?";
+var baseURL_hotels = "https://booking-com.p.rapidapi.com/v1/hotels/search?";
 var base_hotelPhoto = "https://apidojo-booking-v1.p.rapidapi.com/properties/get-hotel-photos?";
 
 const settings = {
@@ -13,8 +13,8 @@ const settings = {
 	"crossDomain": true,
 	"method": "GET",
 	"headers": {
-		"X-RapidAPI-Key": "c1f202a7cbmsh8b90010ef43997bp1724c4jsna7e8b9d9879c",
-		"X-RapidAPI-Host": "apidojo-booking-v1.p.rapidapi.com"
+		"X-RapidAPI-Key": "b9ecd272f1mshf60584f0c72b623p1c55a0jsn6053571d69cf",
+		"X-RapidAPI-Host": "booking-com.p.rapidapi.com"
 	}
 };
 
@@ -23,8 +23,8 @@ getDestInfo();
 //Get destination info
 function getDestInfo() {
     var queryParams_desInfo = {
-        text: des,
-        languagecode: "en-us"
+        name: des,
+        locale: "en-gb"
     };
     settings.url = baseURL_desInfo + $.param(queryParams_desInfo);
     $.ajax(settings).done(function (response) {
@@ -41,46 +41,53 @@ function getDestInfo() {
 // Get hotels Info
 function getHotels(desInfo) {
     var queryParams_hotels = {
-        offset: 0,
-        guest_qty: 2,
-        room_qty:1,
-        price_filter_currencycode:"USD",
+        units:"metric",
+        adults_number: 2,
+        room_number:1,
         order_by:"popularity",
-        languagecode:"en-us",
-        travel_purpose:"leisure"
+        filter_by_currency:"AED",
+        locale:"en-gb"
     };
-    queryParams_hotels.arrival_date = arrival_date;
-    queryParams_hotels.departure_date = departure_date;
-    queryParams_hotels.dest_ids = desInfo.dest_id;
-    queryParams_hotels.search_type = desInfo.dest_type;
+    queryParams_hotels.checkin_date = arrival_date;
+    queryParams_hotels.checkout_date = departure_date;
+    queryParams_hotels.dest_id = desInfo.dest_id;
+    queryParams_hotels.dest_type = desInfo.dest_type;
     settings.url = baseURL_hotels + $.param(queryParams_hotels);
+    console.log(settings.url);
     $.ajax(settings).done(function (response) {
         for (var i = 0; i < 9; i++) {// get the first 9 hotels info
             var hotel = {
                 hotel_name: response.result[i].hotel_name,
                 address:response.result[i].address,
                 hotel_id : response.result[i].hotel_id,
-                review_score : response.result[i].review_score
+                hotel_image: response.result[i].max_photo_url,
+                review_score : response.result[i].review_score,
+                gross_price: response.result[i].gross_price,
+                currency: response.result[i].currency
             };
-            getHotelImage(response.result[i].hotel_id, hotel);
+            hotels.push(hotel);
         }
-        console.log(hotels);
+        displayHotels();
     });
     
 }
 
-//Get hotel hi res image
-function getHotelImage(hotelID, hotel) {
-    var queryParams_hotelPhoto = {
-        hotel_ids:hotelID
-    };
-    settings.url = base_hotelPhoto + $.param(queryParams_hotelPhoto);
-    $.ajax(settings).done(function (imgData) {
-        var image = "https://cf.bstatic.com"+imgData.data[hotelID][0][4];
-        hotel.hotel_image = image;
-        hotels.push(hotel);
-    });  
+function displayHotels() {
+    var hotel_list = "";
+    for (var i = 0; i < hotels.length; i++) {
+        hotel_list += '<div class="card col-3">';
+        hotel_list += '<img src="'+hotels[i].hotel_image+'" class="card-img-top" alt="image of '+hotels[i].hotel_name+'">';
+        hotel_list += '<div class="card-body">';
+        hotel_list += '<h5 class="card-title">'+hotels[i].hotel_name+'</h5>';
+        hotel_list += '<p class="card-text">'+hotels[i].address+'</p>';
+        hotel_list += '<p class="card-text">'+hotels[i]. review_score+'</p>';
+        hotel_list += '</div>';
+        hotel_list += '</div>';
+        console.log(hotel_list);
+    }
+    $("#hotels").html(hotel_list);
 }
+
 
 
 
